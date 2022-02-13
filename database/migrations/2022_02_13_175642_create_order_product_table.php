2bc255a365b933cf9ef3cc5bed9ea4bcef0a2363
Charles Muojekwu <charlesmuojekwu@gmail.com>
2022-02-13 19:07:52 +0100

    [migrations] created product,order,categories migration with pivot tables

--- database/migrations/2022_02_13_175642_create_order_product_table.php
+++ database/migrations/2022_02_13_175642_create_order_product_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOrdersTable extends Migration
+class CreateOrderProductTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,11 @@ class CreateOrdersTable extends Migration
      */
     public function up()
     {
-        Schema::create('orders', function (Blueprint $table) {
-            $table->id();
-            $table->integer('user_id');
-            $table->string('transaction_id');
-            $table->integer('total')->default(0);
-            $table->timestamps();
+        Schema::create('order_product', function (Blueprint $table) {
+            $table->integer('order_id');
+            $table->integer('product_id');
+            $table->integer('quantity')->default(0);
+
         });
     }
 
@@ -29,6 +28,6 @@ class CreateOrdersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('orders');
+        Schema::dropIfExists('order_product');
     }
 }
--- database/migrations/2022_02_13_175725_create_category_product_table.php
+++ database/migrations/2022_02_13_175725_create_category_product_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOrdersTable extends Migration
+class CreateCategoryProductTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,9 @@ class CreateOrdersTable extends Migration
      */
     public function up()
     {
-        Schema::create('orders', function (Blueprint $table) {
-            $table->id();
-            $table->integer('user_id');
-            $table->string('transaction_id');
-            $table->integer('total')->default(0);
-            $table->timestamps();
+        Schema::create('category_product', function (Blueprint $table) {
+            $table->integer('category_id');
+            $table->integer('product_id');
         });
     }
 
@@ -29,6 +26,6 @@ class CreateOrdersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('orders');
+        Schema::dropIfExists('category_product');
     }
 }
