f4e54af49e880a4cc3dc783851c0e88da2994368
Charles Muojekwu <charlesmuojekwu@gmail.com>
2022-02-18 08:56:38 +0100

    [controller] aded category and project created view

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,3 +24,8 @@ Route::post('/purchase', [UserController::class, 'purchase']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('category', [CategoryController::class, 'index']);
+
+//Route::apiResource('categories', CategoryController::class);
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('app');
 });
+
+
 
 /// redirect any other url
 Route::any('/{any}', function ($id) {
